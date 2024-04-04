33539b717cf4e44645ff179f9cc2bb29e1a9506b
Gary Johnson <gary.johnson@leasequery.com>
2024-04-04 13:35:47 -0400

    Remove comment

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -124,7 +124,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.erb_format.with({
-      extra_args = { "--print-width=120" } -- avoid aggressive wrapping, wrapped ERB can be particularly ugly
+      extra_args = { "--print-width=120" }
     }),
     null_ls.builtins.diagnostics.erb_lint,
   },
