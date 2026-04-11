1e923e3617ee0fac2e01fe214bd414a3723ddb4f
Gary Johnson <gary@usefulbits.io>
2026-04-11 17:00:08 -0400

    Updating astronvim to v6

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -6,7 +6,7 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   -- use mason-lspconfig to configure LSP installations
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = {
